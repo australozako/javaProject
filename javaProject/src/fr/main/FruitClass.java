8741740e00625ffeb0e77a60b3409b31a8b18ab3
zako <zakofr12@gmail.com>
2014-03-13 10:51:44 +0100

    add class FruitClass

---
+++ javaProject/src/fr/main/FruitClass.java
@@ -0,0 +1,25 @@
+package fr.main;
+
+public class FruitClass {
+	private String variete;
+	private String origin;
+
+	/* Getters and Setters */
+
+	public String getVariete() {
+		return variete;
+	}
+
+	public void setVariete(String variete) {
+		this.variete = variete;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+}
